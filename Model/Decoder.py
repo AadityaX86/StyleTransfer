fixed normalization and removed artifacts

Model/Decoder.py
@@ -59,7 +59,8 @@ class Decoder(nn.Module):
             B, N, C = x.size()
             (H, W) = input_resolution
             x = x.permute(0, 2, 1).reshape(B, C, H, W)
-            x = self.decoder(x)  
+            x = self.decoder(x) 
+            x = nn.Tanh()(x) 
             return x
 
 # Testing
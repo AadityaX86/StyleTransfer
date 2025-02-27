6cce77b95eab32274c6c5e79205c4896db3a4929
AnkitNeupane007 <ankit.net60@gmail.com>
2025-02-27 19:05:19 +0545

    fixed normalization and removed artifacts

--- Model/Decoder.py
+++ Model/Decoder.py
@@ -59,7 +59,8 @@ class Decoder(nn.Module):
             B, N, C = x.size()
             (H, W) = input_resolution
             x = x.permute(0, 2, 1).reshape(B, C, H, W)
-            x = self.decoder(x)  
+            x = self.decoder(x) 
+            x = nn.Tanh()(x) 
             return x
 
 # Testing
